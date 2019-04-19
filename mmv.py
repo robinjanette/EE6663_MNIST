18de58e1e05d329f9e9eb8d47bed86d8b0adf54a
JTEnglish <je787413@ohio.edu>
2019-04-19 15:26:08 -0400

    reduce memory usage - thanks, Dave

--- mmv.py
+++ mmv.py
@@ -6,22 +6,17 @@ def format():
 
     # label data and scale from (0,255) to (0,1)
     labeled = [[]] * 10 # 10 digits
-    for i in range(30000):
+    for i in range(len(data)):
         labeled[labels[i]].append(data[i] / 255)
-    
-    # stack pixels together with column_stack over the images per digit 
-    for digit in range(len(labeled)):
-        labeled[digit] = np.column_stack(labeled[digit])
     
     return labeled
 
 def mmv(formatted):
     mmv_data = [{'mean': np.array([]), 'median': np.array([]), 'var': np.array([])}]*10
     for digit in range(10): # foreach digit
-        for i in range(784): # foreach pixel
-            mmv_data[digit]['mean'] = np.append(mmv_data[digit]['mean'], np.mean(formatted[digit][i]))
-            mmv_data[digit]['median'] = np.append(mmv_data[digit]['median'], np.median(formatted[digit][i]))
-            mmv_data[digit]['var'] = np.append(mmv_data[digit]['var'], np.var(formatted[digit][i]))
+        mmv_data[digit]['mean'] = np.mean(formatted[digit], axis=0)
+        mmv_data[digit]['median'] = np.median(formatted[digit], axis=0)
+        mmv_data[digit]['var'] = np.var(formatted[digit], axis=0)
 
-    print(list(mmv_data[0]['mean']))
+    return mmv_data
 
