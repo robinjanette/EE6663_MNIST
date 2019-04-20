97408948b4e49c48ad435d04728c885f96280f60
leydern <49655964+leydern@users.noreply.github.com>
2019-04-19 23:28:18 -0400

    Adding fixed apply weights
    Fixed the code and changed the name.
    
    applyWeights(weights, test)
    Returns -1 if they dot product can't happen
    otherwise it returns the dot product

---
+++ applyWeights.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+# Takes the weight matrix and a test smaple then computes the dot product
+# of the weights and test smaple
+# If the weights and test sizes don't match return -1
+def applyWeights(weights, test):
+
+    # Check if the inputs are correct
+    if len(weights[0]) != len(test):
+        return -1
+
+    return np.dot(weights,test)
+
+
+
+##l = np.array([[0,0,0,0,0],[1,20,3,4,5]])
+##m = np.array([1,0,0,0,0])
+##
+##res = applyWeights(l,m.T)
+##
+##print (res)
