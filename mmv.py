1a055f7c7a9a965fda96a814be9fb7319f49afb0
JTEnglish <je787413@ohio.edu>
2019-04-20 13:23:29 -0400

    fixed list pointer bug, added output image script for mean and median

--- mmv.py
+++ mmv.py
@@ -4,19 +4,31 @@ import numpy as np
 def format():
     data, labels, _, _ = mnist.load()
 
+    labels = list(labels)
+
     # label data and scale from (0,255) to (0,1)
-    labeled = [[]] * 10 # 10 digits
+    labeled = [[], [], [], [], [],
+               [], [], [], [], []]
     for i in range(len(data)):
         labeled[labels[i]].append(data[i] / 255)
     
     return labeled
 
 def mmv(formatted):
-    mmv_data = [{'mean': np.array([]), 'median': np.array([]), 'var': np.array([])}]*10
-    for digit in range(10): # foreach digit
+    mmv_data = [{'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])},
+                {'mean': np.array([]), 'median': np.array([]), 'var': np.array([])}]
+                
+    for digit in range(10):
         mmv_data[digit]['mean'] = np.mean(formatted[digit], axis=0)
         mmv_data[digit]['median'] = np.median(formatted[digit], axis=0)
         mmv_data[digit]['var'] = np.var(formatted[digit], axis=0)
 
     return mmv_data
-
---
+++ output-mean-median.py
@@ -0,0 +1,32 @@
+import mmv
+import numpy as np
+import cv2
+
+f = mmv.format()
+m = mmv.mmv(f)
+print('MNIST MMV Done.')
+
+
+for num in range(10):
+    m_img = np.reshape(m[num]['median'], (28, 28))
+
+    width = int(m_img.shape[1] * 10)
+    height = int(m_img.shape[0] * 10)
+    dim = (width, height)
+    # resize image
+    resized = cv2.resize(m_img, dim, interpolation = cv2.INTER_AREA) 
+
+    cv2.imwrite('images/' + str(num) + '-MEDIAN.jpg', resized)
+
+    # cv2.imshow(str(num) + '-MEDIAN', resized)
+
+    m_img = np.reshape(m[num]['mean'], (28, 28))
+
+    width = int(m_img.shape[1] * 10)
+    height = int(m_img.shape[0] * 10)
+    dim = (width, height)
+    # resize image
+    resized = cv2.resize(m_img, dim, interpolation = cv2.INTER_AREA) 
+
+    # cv2.imshow(str(num) + '-MEAN', resized)
+    cv2.imwrite('images/' + str(num) + '-MEAN.jpg', resized)
